4957c1ba3435b7002df274d659fb404ac389090d
Kim Nguyen <nnhk23@gmail.com>
2020-10-14 11:40:26 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+    attr_accessor :first_name, :last_name, :knowledge
+
+    def initialize
+        @first_name = first_name
+        @last_name = last_name
+        @knowledge = []
+    end
+
+    def learn(info)
+        @knowledge << info
+    end
 
 end
